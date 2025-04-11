f7b788d90863fa09d7b6c263d4404c2ee2345c2e
gam3boy1 <aadiananddeveloper05@gmail.com>
2025-04-11 19:30:03 -0400

    created server endpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
+"use client"
+
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("/api/hello");
+      const text = await res.text();
+      console.log("TEXT: " + text);
+    };
+
+    fetchData();
+  }, [])
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
