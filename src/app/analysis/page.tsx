5164c916973948a7bf010e559125850065fc134a
bobbybotbop <bobbybotbop@gmail.com>
2025-04-12 12:56:31 -0400

    updated analysis component

--- src/app/analysis/page.tsx
+++ src/app/analysis/page.tsx
@@ -21,35 +21,10 @@ export default function AnalysisPage() {
   const [clicked, setClicked] = useState(0)
 
   return (
-    <div className="w-screen h-screen">
-      <div className="flex justify-center items-center h-12">
-        {/* Top-left element */}
-        <Button className="absolute left-2" variant="ghost" size="icon">
-          <ChevronLeft style={{ scale: 2 }} />
-        </Button>
-        <p className="text-4xl mt-[-5px]">Analysis:</p>
-      </div>
+    
       <main>
         <Analysis cleaned={true} designed={true} visualized={true} analyzed={true} />
-        
       </main>
-      <footer className="bg-gray-200 w-full h-14 row-start-3 flex items-center justify-center absolute bottom-0">
-        <a
-          className="gap-2 text-gray-600 flex items-center hover:underline hover:underline-offset-4"
-          href="https://www.aadiananddeveloper05.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/github-logo.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          GitHub Repository
-        </a>
-      </footer>
-    </div>
+      
   );
 }
--- src/components/analysis.tsx
+++ src/components/analysis.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { useState } from "react";
 import DropDown from "@/components/ui/dropDown";
+import { Button } from "@/components/ui/button";
+import { ChevronLeft } from "lucide-react";
+
 
 //import { useRouter } from 'next/navigation'
 
@@ -21,13 +24,24 @@ export default function Analysis({
   const tempData = "tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData tempData  "
 
   return (
+    <div>
+      <div className="w-screen h-screen bg-red-500">
+        <div className="flex justify-center items-center h-12">
+          <Button className="absolute left-2" variant="ghost" size="icon">
+            <ChevronLeft style={{ scale: 2 }} />
+          </Button>
+          <p className="text-4xl mt-[-5px]">Analysis:</p>
+        </div>
+      </div>
     <main>
       
       <button
         className="bg-blue-500 text-white px-4 py-2 rounded"
         onClick={() => setClicked(clicked + 1)}>
-        Click me
+        clean
       </button>
+      
+
         {/* temporarly based on clicked variable need to add ani*/}
       <DropDown text="Cleaning data" clicked={clicked} phaseNum={0} data={tempData}/>
       {clicked>0 && <DropDown text="Finding Patterns" clicked={clicked} phaseNum={1} data={tempData}/>}
@@ -35,5 +49,8 @@ export default function Analysis({
       {clicked>2 && <DropDown text="Found Data!" clicked={clicked} phaseNum={3} data={tempData}/>}
               
     </main>
+    </div>
+    
+    
   );
 }
