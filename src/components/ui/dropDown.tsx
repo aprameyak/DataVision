4a8661bda7996a6dc3de844541fd75974a50c55a
TN0123 <112415698+TN0123@users.noreply.github.com>
2025-04-12 11:51:34 -0400

    added analysis component

--- src/components/ui/dropDown.tsx
+++ src/components/ui/dropDown.tsx
@@ -1,22 +1,28 @@
-import { ChevronDown } from 'lucide-react';
+import { ChevronDown } from "lucide-react";
 
 interface StatTestIndicatorProps {
-    text: string
-    clicked: number
-    phaseNum: number //0 is first step
+  text: string;
+  clicked: number;
+  phaseNum: number; //0 is first step
+}
 
-  }
+const offset = 6;
 
-const offset = 6
-
-export default function dropDown({text, clicked, phaseNum}: StatTestIndicatorProps) {
-    return(
-     <div>
-        <p className={` flex flex-col absolute left-10 clicked>2 ?"text-[17px]"
-            :"loading"`}>
+export default function dropDown({
+  text,
+  clicked,
+  phaseNum,
+}: StatTestIndicatorProps) {
+  return (
+    <div>
+      <p
+        className={` flex flex-col w-full absolute left-10 clicked>2 ?"text-[17px]"
+            :"loading"`}
+      >
         {`${text} ${clicked}`}
-        </p>
-        <ChevronDown className="absolute left-15" size={50}/>
-        {/* {phaseNum > 0 && clicked > phaseNum && } */}
-    </div>)
+      </p>
+      <ChevronDown className="absolute left-15" size={50} />
+      {/* {phaseNum > 0 && clicked > phaseNum && } */}
+    </div>
+  );
 }
