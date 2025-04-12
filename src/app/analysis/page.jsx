cc3a3446583dbd62cd45d2956f930b6ee00cdcd8
bobbybotbop <bobbybotbop@gmail.com>
2025-04-11 21:05:34 -0400

    Update page.jsx

--- src/app/analysis/page.jsx
+++ src/app/analysis/page.jsx
@@ -1,9 +1,9 @@
 export default function Analysis() {
   return (
-    <div className="bg-white grid grid-rows-[56px_1fr_56px] items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+    <div className="min-h-screen relative">
       {" "}
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <p className="text-5xl">Analysis:</p>
+      <main className="mt-2 text-center">
+        <p className="text-4xl">Analysis:</p>
       </main>
     </div>
   );
