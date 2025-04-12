c9a0f65b93b4d2cfe05718beb799ec029dc4f094
bobbybotbop <bobbybotbop@gmail.com>
2025-04-11 20:58:12 -0400

    add analysis page

---
+++ src/app/analysis/page.jsx
@@ -0,0 +1,10 @@
+export default function Analysis() {
+  return (
+    <div className="bg-white grid grid-rows-[56px_1fr_56px] items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+      {" "}
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <p className="text-5xl">Analysis:</p>
+      </main>
+    </div>
+  );
+}
